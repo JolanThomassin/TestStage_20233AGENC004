249a14d196e56860b0633d9f1cc41d7c6fad9c3f
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2023-07-18 19:31:14 -0400

    Tâche 3, recherche sur la vue score #3
    Pour éviter toute incertitude quant à la nécessité de rechercher dans crawl.html_content ou crawl.title, j'ai pris l'initiative de créer les deux codes afin de m'assurer de couvrir toutes les possibilités.
    
    Issue traité : #3

---
+++ JolanTh_20233AGENC004_RechercheHTMLContent.sql
@@ -0,0 +1,17 @@
+-- Suppression de la fonction "recherche" existante
+DROP FUNCTION IF EXISTS recherche(TEXT);
+
+-- Création de la nouvelle fonction "recherche"
+CREATE FUNCTION recherche(keyword TEXT) RETURNS TABLE (document_id UUID, score NUMERIC) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT s.id::UUID, CAST(s.score AS NUMERIC)
+  FROM "jolan.thomassin.1@ens.etsmtl.ca".score AS s
+  WHERE s.html_content ILIKE '%' || keyword || '%'
+  ORDER BY s.score DESC
+  LIMIT 10;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Recherche avec le mot-clé : "Canada"
+SELECT * FROM recherche('Canada');
